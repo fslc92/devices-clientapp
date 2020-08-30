0eef012f3f80f389731ef0b309b58e19ebaa4dd4
Sebastian Lozada <fslc92@gmail.com>
2020-08-30 16:26:56 -0500

    add global config

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+export default {
+  devicesApiUrl: "http://localhost:3000",
+  deviceTypes: [
+    { key: "MAC", label: "Mac" },
+    { key: "WINDOWS_SERVER", label: "Windows Server" },
+    { key: "WINDOWS_WORKSTATION", label: "Windows Workstation" },
+  ],
+};
