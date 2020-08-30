2535655da67bcf446421bfe8aaddd7eacde5c6f0
Sebastian Lozada <fslc92@gmail.com>
2020-08-30 16:27:15 -0500

    add menu

---
+++ src/components/Menu.js
@@ -0,0 +1,15 @@
+import React, { Children } from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import "./Menu.css";
+
+export default ({ children }) => {
+  return (
+    <>
+      <Jumbotron className="masthead">
+        <h1>Ninja RMM Device Manager</h1>
+      </Jumbotron>
+      <Container>{children}</Container>
+    </>
+  );
+};
